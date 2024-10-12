0888c4b67ee889a13f9d505cd3064a83ca49013e
Jayakrishna-Developer <rj519669@gmail.com>
2024-10-12 14:30:48 +0530

    changedcss

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,27 +1,35 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import Add from '../Components/Add'
-import Category from '../Components/Category'
-import View from '../Components/View';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Add from "../Components/Add";
+import Category from "../Components/Category";
+import View from "../Components/View";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHistory } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
-const[uploadVideoResponse,setUploadVideoResponse]=useState({})
-const[dropvideoResponse,setDropVideoResponse]=useState({})
+  const [uploadVideoResponse, setUploadVideoResponse] = useState({});
+  const [dropvideoResponse, setDropVideoResponse] = useState({});
 
   return (
     <div>
-      <div className="container  mt-5 mb-5 d-flex justify-content-between">
+      <div className="container mt-5 mb-5 d-flex justify-content-between">
         <div className="add-videos">
           <Add setUploadVideoResponse={setUploadVideoResponse} />
         </div>
-        <Link to={"./watch-history"}>Watch-History</Link>
+        <Link
+          to={"./watch-history"}
+          style={{ textDecoration: "none", color: "#2c3e50" }}
+        >
+          <FontAwesomeIcon icon={faHistory} style={{ marginRight: "5px" }} />
+          Watch History
+        </Link>
       </div>
 
-      <div className="container-fluid w-100 mt-5 mb-5  row">
+      <div className="container-fluid w-100 mt-5 mb-5 row">
         <div className="n"></div>
         <div
           style={{ marginBottom: "200px" }}
-          className=" all-videos col-lg-8  col-md-12 col-sm-12 col-12"
+          className="all-videos col-lg-8 col-md-12 col-sm-12 col-12"
         >
           <h1>All videos</h1>
           <View
@@ -38,4 +46,4 @@ const[dropvideoResponse,setDropVideoResponse]=useState({})
   );
 }
 
-export default Home
+export default Home;
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -16,10 +16,10 @@ function LandingPage() {
             Welcome to <span style={{ color: "#4d96b6" }}>Media Player</span>
           </h1>
           <p style={{ textAlign: "justify" }}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet
-            voluptatum aspernatur ullam hic cum quasi rerum ex doloribus ad
-            quaerat eligendi fugiat excepturi itaque voluptas alias totam, quod
-            delectus accusantium.
+            Explore a new way to manage, play, and organize your media content
+            with ease. Our media player offers seamless functionality,
+            cross-platform accessibility, and powerful performance, ensuring the
+            best experience for your entertainment needs.
           </p>
           <button
             onClick={() => navigateByUrl("/home")}
@@ -45,7 +45,7 @@ function LandingPage() {
         >
           <img
             src="https://media0.giphy.com/media/SYe6yLqC1RWec/giphy-downsized.gif"
-            alt="Media Player GIF"
+            alt="Media player"
             className="img-fluid"
           />
         </Col>
@@ -54,69 +54,94 @@ function LandingPage() {
 
       <div
         className="container-fluid"
-        style={{ backgroundColor: "#e6eff2", padding: "50px 0" }}
+        style={{ backgroundColor: "#f0f4f7", padding: "50px 0" }}
       >
-        <h3 className="text-center mt-5 mb-5">Features</h3>
+        <h3 className="text-center mt-5 mb-5" style={{ color: "#2c3e50" }}>
+          Features
+        </h3>
+
         <Row>
           <Col lg={4} md={6} sm={12}>
             <Card
-              className="feature-card mb-3"
-              style={{ border: "2px solid #a6b1c0" }}
+              className="feature-card mb-4"
+              style={{
+                border: "2px solid #a6b1c0",
+                backgroundColor: "#ffffff",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                borderRadius: "10px",
+              }}
             >
               <Card.Img
                 variant="top"
-                src="https://media.giphy.com/media/jYGWqorFl75tK/giphy.gif"
-                alt="Feature 1"
+                src="https://cdn.dribbble.com/users/740188/screenshots/11467544/media/c767c489d2ee39590d36a5234c5eaedc.gif"
+                alt="Managing Videos"
+                style={{ borderRadius: "10px 10px 0 0" }}
               />
               <Card.Body>
-                <Card.Title style={{ color: "#4d96b6" }}>
-                  Managing Videos
+                <Card.Title style={{ color: "#4d96b6", fontWeight: "bold" }}>
+                  MANAGING VIDEOS
                 </Card.Title>
-                <Card.Text style={{ color: "#2c3e50" }}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                  quis.
+                <Card.Text style={{ color: "#2c3e50", textAlign: "justify" }}>
+                  Organize your media library effortlessly with categorized
+                  playlists and seamless search functionality. Create, delete,
+                  and edit your playlists with a simple, intuitive interface.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
+
           <Col lg={4} md={6} sm={12}>
             <Card
-              className="feature-card mb-3"
-              style={{ border: "2px solid #a6b1c0" }}
+              className="feature-card mb-4"
+              style={{
+                border: "2px solid #a6b1c0",
+                backgroundColor: "#ffffff",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                borderRadius: "10px",
+              }}
             >
               <Card.Img
                 variant="top"
-                src="https://media.giphy.com/media/jYGWqorFl75tK/giphy.gif"
-                alt="Feature 2"
+                src="https://cdn.dribbble.com/users/1241808/screenshots/2845056/lc03.gif"
+                alt="Categorized Content"
+                style={{ borderRadius: "10px 10px 0 0" }}
               />
               <Card.Body>
-                <Card.Title style={{ color: "#4d96b6" }}>
-                  Categorized
+                <Card.Title style={{ color: "#4d96b6", fontWeight: "bold" }}>
+                  CATEGORIZED
                 </Card.Title>
-                <Card.Text style={{ color: "#2c3e50" }}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                  quis.
+                <Card.Text style={{ color: "#2c3e50", textAlign: "justify" }}>
+                  Organize your videos into categories for easier navigation and
+                  access. Easily manage your playlists with a few clicks. making it even simpler to find content you love.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
+
           <Col lg={4} md={6} sm={12}>
             <Card
-              className="feature-card mb-3"
-              style={{ border: "2px solid #a6b1c0" }}
+              className="feature-card mb-4"
+              style={{
+                border: "2px solid #a6b1c0",
+                backgroundColor: "#ffffff",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                borderRadius: "10px",
+              }}
             >
               <Card.Img
                 variant="top"
-                src="https://media.giphy.com/media/jYGWqorFl75tK/giphy.gif"
-                alt="Feature 3"
+                src="https://cdn.dribbble.com/users/422171/screenshots/5353624/video_player.gif"
+                alt="Watch History"
+                style={{ borderRadius: "10px 10px 0 0" }}
               />
               <Card.Body>
-                <Card.Title style={{ color: "#4d96b6" }}>
-                  Watch History
+                <Card.Title style={{ color: "#4d96b6", fontWeight: "bold" }}>
+                  WATCH HISTORY
                 </Card.Title>
-                <Card.Text style={{ color: "#2c3e50" }}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                  quis.
+                <Card.Text style={{ color: "#2c3e50", textAlign: "justify" }}>
+                  Sync your watch history across devices and never lose track of
+                  what you've watched. Enjoy seamless transitions between
+                  devices while maintaining your video progress.
                 </Card.Text>
               </Card.Body>
             </Card>
@@ -130,24 +155,42 @@ function LandingPage() {
           backgroundColor: "#daeaf6",
           padding: "50px 0",
           color: "#2c3e50",
+          margin: "0",
         }}
       >
         <div className="col-lg-5 col-md-8 col-sm-12 mb-4">
           <h4 style={{ color: "#2c3e50" }}>Simple, Powerful & Fast</h4>
-          <h6 className="mb-5 mt-3">
-            <span style={{ color: "#4d96b6" }}>Play Everything</span> Lorem
-            ipsum dolor sit, amet consectetur adipisicing elit. Expedita culpa
-            voluptates error eos ducimus.
+          <h6 className="mb-5 mt-3" style={{ textAlign: "justify" }}>
+            <span style={{ color: "#4d96b6" }}>Play Everything</span> – Our
+            media player supports an extensive range of video formats including
+            MP4, AVI, MKV, and more, ensuring that you can enjoy any video
+            content without worrying about compatibility issues. With a
+            user-friendly interface, playing your favorite videos is just a
+            click away.
           </h6>
-          <h6 className="mb-5 mt-3">
-            <span style={{ color: "#4d96b6" }}>Categorize Videos</span> Lorem
-            ipsum dolor sit, amet consectetur adipisicing elit. Expedita culpa
-            voluptates error eos ducimus.
+          <h6 className="mb-5 mt-3" style={{ textAlign: "justify" }}>
+            <span style={{ color: "#4d96b6" }}>Effortless Organization</span> –
+            Categorizing your video library has never been easier. Our intuitive
+            categorization tools allow you to sort your videos into playlists,
+            genres, or custom folders. Whether you want to organize by
+            favorites, most watched, or recently added, it's all streamlined for
+            your convenience.
           </h6>
-          <h6 className="mb-5 mt-3">
-            <span style={{ color: "#4d96b6" }}>Managing Videos</span> Lorem
-            ipsum dolor sit, amet consectetur adipisicing elit. Expedita culpa
-            voluptates error eos ducimus.
+          <h6 className="mb-5 mt-3" style={{ textAlign: "justify" }}>
+            <span style={{ color: "#4d96b6" }}>Lightning-Fast Performance</span>{" "}
+            – Experience smooth playback and fast loading times. With advanced
+            optimization techniques, our media player ensures your videos start
+            playing instantly, even in high-definition formats. No more waiting
+            for buffering, just instant enjoyment of your content.
+          </h6>
+          <h6 className="mb-5 mt-3" style={{ textAlign: "justify" }}>
+            <span style={{ color: "#4d96b6" }}>
+              Cross-Platform Accessibility
+            </span>{" "}
+            – Use the media player on any device, whether you're on your laptop,
+            tablet, or smartphone. Sync your video library across platforms, so
+            you can pick up where you left off, no matter which device you're
+            using.
           </h6>
         </div>
 
